ba37c1dca231aff5fe281278dc3142cf6c8b8d5a
Karl Yerkes <karl.yerkes@gmail.com>
2023-02-07 12:01:15 -0800

    in class

--- Delay.cpp
+++ Delay.cpp
@@ -5,6 +5,8 @@
 
 #include <juce_audio_processors/juce_audio_processors.h>
 
+#include <vector>
+
 template <typename T>
 T mtof(T m) {
   return T(440) * pow(T(2), (m - T(69)) / T(12));
@@ -15,10 +17,45 @@ T dbtoa(T db) {
   return pow(T(10), db / T(20));
 }
 
+class DelayLine : std::vector<float> {
+  //
+  int index = 0;
+
+ public:
+  float read(float seconds_ago, float samplerate) {
+    //
+    jassert(seconds_ago < size() / samplerate);
+
+    float i = index - seconds_ago * samplerate;
+    if (i < 0) {
+      i += size();
+    }
+    return at((int)i);  // no linear interpolation
+  }
+
+  void write(float value) {
+    jassert(size() > 0);
+    at(index) = value;  // overwrite the oldest value
+
+    // handle the wrapping for circular buffer
+    index++;
+    if (index >= size()) index = 0;
+  }
+
+  void allocate(float seconds, float samplerate) {
+    // floor(seconds * samplerate) + 1 samples
+    resize((int)floor(seconds * samplerate) + 1);
+  }
+};
+
 class Delay : public juce::AudioProcessor {
   juce::AudioParameterFloat* gain;
-  juce::AudioParameterFloat* delay;
+  juce::AudioParameterFloat* delay_time;
+  juce::AudioParameterChoice* choice;
+
   /// add parameters here ///////////////////////////////////////////////////
+
+  DelayLine delay_line;
 
  public:
   Delay()
@@ -29,12 +66,19 @@ class Delay : public juce::AudioProcessor {
     addParameter(gain = new juce::AudioParameterFloat(
                      {"gain", 1}, "Gain",
                      juce::NormalisableRange<float>(-65, -1, 0.01f), -65));
-    addParameter(delay = new juce::AudioParameterFloat(
+    addParameter(delay_time = new juce::AudioParameterFloat(
                      {"delay", 1}, "Delay",
                      juce::NormalisableRange<float>(0, 4, 0.01f), 0.7f));
+    addParameter(choice = new juce::AudioParameterChoice(
+                     "choice", "Choice", juce::StringArray({"Foo", "Bar"}), 0));
     /// add parameters here /////////////////////////////////////////////
 
     // XXX getSampleRate() is not valid here
+
+    // delay_line.allocate(delay_time->getNormalisableRange().end,
+    //                     (float)getSampleRate());
+
+    // std::cout << delay_time->getNormalisableRange().end << std::endl;
   }
 
   /// handling the actual audio! ////////////////////////////////////////////
@@ -44,8 +88,11 @@ class Delay : public juce::AudioProcessor {
     auto left = buffer.getWritePointer(0, 0);
     auto right = buffer.getWritePointer(1, 0);
 
+    //   printf("got here\n"); // i/o ~ might take a while
+
     for (int i = 0; i < buffer.getNumSamples(); ++i) {
-      left[i] = 0;
+      delay_line.write((left[i] + right[i]) / 2);
+      left[i] = delay_line.read(delay_time->get(), (float)getSampleRate());
       right[i] = left[i];
     }
   }
@@ -56,8 +103,10 @@ class Delay : public juce::AudioProcessor {
   // }
 
   /// start and shutdown callbacks///////////////////////////////////////////
-  void prepareToPlay(double, int) override {
+  void prepareToPlay(double samplerate, int) override {
     // XXX when does this get called? seems to not get called in stand-alone
+    delay_line.allocate(delay_time->getNormalisableRange().end,
+                        (float)samplerate);
   }
   void releaseResources() override {}
 
